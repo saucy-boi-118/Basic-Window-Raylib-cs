60c464a27daf9ec4a3cdcfee1b1bc0eeaf060921
ripe_onions <mahir.muzaddid2025@gmail.com>
2026-03-09 15:37:25 -0700

    Update 2D.cs

--- 2D.cs
+++ 2D.cs
@@ -1,1 +1,37 @@
+using System;
+using Raylib_cs;
+using System.Numerics;
+
+class Program
+{
+public static void Main()
+        {
+            // initialize
+            const int WINSIZE = 500;
+
+            Raylib.InitWindow(WINSIZE, WINSIZE, "Basic 2D");
+
+            Raylib.SetTargetFPS(60);
+
+            Vector2 circPos = new(250, 250);
+
+            while(!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+
+
+                Raylib.ClearBackground(Color.RayWhite);
+
+                Raylib.DrawCircleV(circPos, 15, Color.Beige);
+
+                Raylib.DrawText("Hello World!", 200, 250, 25, Color.Black);
+
+
+                Raylib.EndDrawing();
+            }
+
+            // Unloading
+            Raylib.CloseWindow();
+        }        
+}
 
